8109678068009934be15eadc467e0b4daf13e8e8
Victoriya Bugakova <victoriya.bg@gmail.com>
2022-06-14 18:33:29 +0300

    react 5

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,6 +1,6 @@
 export default function PageNotFound() {
     return (
-        <div className="not-found__container _container">
+        <div className="not-found not-found__container _container">
             <img className ="not-found__img _img" src="img/page_no_found.gif" alt=""/>
             <h1>Page Not Found</h1>
         </div>
